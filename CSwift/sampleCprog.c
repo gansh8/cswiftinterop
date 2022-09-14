0f6fb6f4a7c2a625bd7b45d0aa0a18bb53a1af41
ganesh.lingam <ganesh.lingam@zohocorp.com>
2022-09-14 16:32:32 +0530

    first commit

---
+++ CSwift/sampleCprog.c
@@ -0,0 +1,15 @@
+//
+//  sampleCprog.c
+//  CSwift
+//
+//  Created by jerin-13862  on 14/09/22.
+//
+
+#include "sampleCprog.h"
+
+void greetSwift();
+
+int product(int multiplier, int multiplicand) {
+	greetSwift();
+	return multiplier * multiplicand;
+}
---
+++ CSwift/sampleCprog.h
@@ -0,0 +1,13 @@
+//
+//  sampleCprog.h
+//  CSwift
+//
+//  Created by jerin-13862  on 14/09/22.
+//
+
+#ifndef sampleCprog_h
+#define sampleCprog_h
+
+#include <stdio.h>
+int product(int multiplier, int multiplicand);
+#endif /* sampleCprog_h */
